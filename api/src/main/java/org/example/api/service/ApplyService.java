e8d7a7efb8f5791ea0be3822a748e95dbd760f02
ojiwon <ojw970725@gmail.com>
2024-12-23 22:46:34 +0900

    feature : Kafka Producer 구현 - topic에 userId 전달 - redis incr로 발급 건수 count

--- api/src/main/java/org/example/api/service/ApplyService.java
+++ api/src/main/java/org/example/api/service/ApplyService.java
@@ -1,6 +1,6 @@
 package org.example.api.service;
 
-import org.example.api.domain.Coupon;
+import org.example.api.producer.CouponCreateProducer;
 import org.example.api.repository.CouponCountRepository;
 import org.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -10,20 +10,23 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
+    private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
-
 
     public void apply(Long userId) {
         long count = couponCountRepository.increment();
 
         if(count > 100) {
+            System.out.println("count > 100 ");
             return;
         }
 
-        couponRepository.save(new Coupon(userId));
+        System.out.println("Send message : " + userId);
+        couponCreateProducer.create(userId);
     }
 }
