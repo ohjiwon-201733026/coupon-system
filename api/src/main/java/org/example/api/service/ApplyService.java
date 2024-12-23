feb6af1b3885963f001bbcc6e40b1eee11ecc2ad
ojiwon <ojw970725@gmail.com>
2024-12-23 22:49:39 +0900

    fix : 원복

--- api/src/main/java/org/example/api/service/ApplyService.java
+++ api/src/main/java/org/example/api/service/ApplyService.java
@@ -1,6 +1,6 @@
 package org.example.api.service;
 
-import org.example.api.producer.CouponCreateProducer;
+import org.example.api.domain.Coupon;
 import org.example.api.repository.CouponCountRepository;
 import org.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -10,12 +10,10 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
-    private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
-        this.couponCreateProducer = couponCreateProducer;
     }
 
     public void apply(Long userId) {
@@ -27,6 +25,6 @@ public class ApplyService {
         }
 
         System.out.println("Send message : " + userId);
-        couponCreateProducer.create(userId);
+        couponRepository.save(new Coupon(userId));
     }
 }
