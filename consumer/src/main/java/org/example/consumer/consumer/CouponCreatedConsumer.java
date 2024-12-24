ed524c113706588d1e39bf1a9b14ba3f4f33eff8
ojiwon <ojw970725@gmail.com>
2024-12-24 21:55:12 +0900

    feature : 컨슈머에서 에러 발생하는 경우 예외 처리 - FailedEvent 저장 - 로그

--- consumer/src/main/java/org/example/consumer/consumer/CouponCreatedConsumer.java
+++ consumer/src/main/java/org/example/consumer/consumer/CouponCreatedConsumer.java
@@ -1,7 +1,11 @@
 package org.example.consumer.consumer;
 
 import org.example.consumer.domain.Coupon;
+import org.example.consumer.domain.FailedEvent;
 import org.example.consumer.repository.CouponRepository;
+import org.example.consumer.repository.FailedEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,15 +13,24 @@ import org.springframework.stereotype.Component;
 public class CouponCreatedConsumer {
 
     private final CouponRepository couponRepository;
+    private final FailedEventRepository failedEventRepository;
+    private final Logger log = LoggerFactory.getLogger(CouponCreatedConsumer.class);
 
-    public CouponCreatedConsumer(CouponRepository couponRepository) {
+    public CouponCreatedConsumer(CouponRepository couponRepository, FailedEventRepository failedEventRepository) {
         this.couponRepository = couponRepository;
+        this.failedEventRepository = failedEventRepository;
     }
 
     @KafkaListener(topics = "coupon_create", groupId = "group_1")
     public void listener(Long userId) {
 
-        System.out.println("userId = " + userId);
-        couponRepository.save(new Coupon(userId));
+        try {
+            System.out.println("userId = " + userId);
+            couponRepository.save(new Coupon(userId));
+        } catch (Exception e) {
+            log.error("failed to create coupon ::" + userId);
+            failedEventRepository.save(new FailedEvent(userId));
+        }
+
     }
 }
