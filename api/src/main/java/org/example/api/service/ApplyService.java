55ab2a9214edd310182c62d8de0e0234590916c4
ojiwon <ojw970725@gmail.com>
2024-12-24 21:47:45 +0900

    feature : 한사람당 1개의 쿠폰만 발급하는 요구사항 추가 - redis set을 사용해 발급받은 사용자 ID 저장

--- api/src/main/java/org/example/api/service/ApplyService.java
+++ api/src/main/java/org/example/api/service/ApplyService.java
@@ -2,6 +2,7 @@ package org.example.api.service;
 
 import org.example.api.domain.Coupon;
 import org.example.api.producer.CouponCreateProducer;
+import org.example.api.repository.AppliedUserRepository;
 import org.example.api.repository.CouponCountRepository;
 import org.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -12,14 +13,23 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if(apply != 1) {
+            System.out.println("이미 발급 ");
+            return;
+        }
+
         long count = couponCountRepository.increment();
 
         if(count > 100) {
